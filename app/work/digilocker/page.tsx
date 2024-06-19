25c502af9b01b6ce73954737974d54e0c9a32b37
Suhelkhan12 <suhel7822@gmail.com>
2024-06-19 16:50:42 +0530

    styles chagne

--- app/work/digilocker/page.tsx
+++ app/work/digilocker/page.tsx
@@ -32,8 +32,26 @@ const page = () => {
   return (
     <>
       <div className=" mt-24">
-        <CsDetail />
-        <div className="h-screen">
+        <div className="pt-8 pb-14">
+          <StandardSection>
+            <div className="">
+              <Image
+                src={"/casestudyImages/digi.svg"}
+                alt="Digilocker logo"
+                width={128}
+                height={100}
+                className=""
+              />
+            </div>
+            <div className="mt-4">
+              <h1 className="">Every document, all at once</h1>
+            </div>
+            <div className="mt-2 text-[1.5rem] lh-140">
+              <p>Reimagining digital documents for a billion Indians</p>
+            </div>
+          </StandardSection>
+        </div>
+        <div className=" min-h-screen">
           <CasestudyVideo />
         </div>
         <CasestudyBottomDetials />
