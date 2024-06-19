e2c76aac2078bebca76231fe129c56086e8da29d
Suhelkhan12 <suhel7822@gmail.com>
2024-06-19 18:22:11 +0530

    delete

--- component/LottiePlayer/LottiePlayer.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import Lottie from "lottie-react";
-
-const LottiePlayer = ({ src }: { src: JSON }) => {
-  return <Lottie animationData={src} loop={true} />;
-};
-
-export default LottiePlayer;
--- component/Tweet/TweetEmbed.tsx
+++
@@ -1,7 +0,0 @@
-import { Tweet } from "react-tweet";
-
-const TweetEmbed = () => {
-  return <Tweet id="1293484000271507458" />;
-};
-
-export default TweetEmbed;
