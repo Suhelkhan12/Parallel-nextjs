277165078e46e2525b4c08adc17aa83a633f1313
Suhelkhan12 <suhel7822@gmail.com>
2024-06-25 11:08:30 +0530

    removed lenis

--- components/Lenis/LenisScroll.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactLenis } from "lenis/react";
-import { ReactNode } from "react";
-
-const LenisScroll = ({ children }: { children: ReactNode }) => {
-  return <ReactLenis root>{children}</ReactLenis>;
-};
-
-export default LenisScroll;
