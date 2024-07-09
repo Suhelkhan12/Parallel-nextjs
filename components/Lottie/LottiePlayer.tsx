02071358ee17433e540026f6e166abe16b6c9e02
Suhelkhan12 <suhel7822@gmail.com>
2024-07-09 11:12:07 +0530

    add intersection observer

--- components/Lottie/LottiePlayer.tsx
+++ components/Lottie/LottiePlayer.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 import Lottie from "lottie-react";
 
 interface LottiePlayerProps {
@@ -6,7 +8,38 @@ interface LottiePlayerProps {
 }
 
 const LottiePlayer = ({ animationData }: LottiePlayerProps) => {
-  return <Lottie animationData={animationData} loop autoplay />;
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 1,
+  });
+  const lottieRef = useRef<any>();
+
+  useEffect(() => {
+    if (inView && lottieRef.current) {
+      lottieRef.current.play();
+    } else if (lottieRef.current) {
+      lottieRef.current.stop();
+    }
+
+    return () => {
+      if (lottieRef.current) {
+        lottieRef.current.destroy();
+      }
+    };
+  }, [inView]);
+
+  return (
+    <div ref={ref}>
+      {inView && (
+        <Lottie
+          animationData={animationData}
+          lottieRef={lottieRef}
+          loop
+          autoplay
+        />
+      )}
+    </div>
+  );
 };
 
 export default LottiePlayer;
